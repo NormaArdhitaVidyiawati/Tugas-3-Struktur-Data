9157c7c925a2b663ee4fd804fffc23111b1605f2
NormaArdhitaVidyiawati <147597223+NormaArdhitaVidyiawati@users.noreply.github.com>
2024-05-14 18:29:50 +0700

    Update Map with Weighted Graph.py

--- Map with Weighted Graph.py
+++ Map with Weighted Graph.py
@@ -1,1 +1,7 @@
+"""
+    Kelompok 6_2023E - Kumpulan Tugas Struktur Data
 
+    1. Tsalist Habibil Muzakki - 23091397163 
+    2. Aditya Revangga D.P - 23091397167 
+    3. Norma Ardhita Vidyiawati - 23091397169
+"""

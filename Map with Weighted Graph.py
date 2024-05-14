4d80470406652a34215337557023bc0a494310c0
NormaArdhitaVidyiawati <147597223+NormaArdhitaVidyiawati@users.noreply.github.com>
2024-05-14 20:09:54 +0700

    Update Map with Weighted Graph.py

--- Map with Weighted Graph.py
+++ Map with Weighted Graph.py
@@ -21,9 +21,24 @@ class KatakanPeta():
 
     def tambahkanKota(self, kota):
         if kota not in self.daftarKota:
+            self.daftarKota[kota] = {}
+            self.jumlahKota += 1
+
+    def tambahkanJalan(self, kota1, cities):
+        for kota in cities:
+            if kota1 and kota in self.daftarKota:
+                self.daftarKota[kota1][kota] = cities[kota]
+                self.daftarKota[kota][kota1] = cities[kota]
+
+    def hapusKota(self, kotaDihapus):
+        if kotaDihapus in self.daftarKota:
+            for kota in self.daftarKota:
+                if kotaDihapus in self.daftarKota[kota]:
+                    del self.daftarKota[kota][kotaDihapus]
+            del self.daftarKota[kotaDihapus]
+            self.jumlahKota -= 1
             
-        
-def hapusJalan(self, kota1, kota2):
+    def hapusJalan(self, kota1, kota2):
         if kota1 and kota2 in self.daftarKota:
             del self.daftarKota[kota1][kota2]
             del self.daftarKota[kota2][kota1]
